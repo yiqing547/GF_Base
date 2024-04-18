93b2f274ffad640ea5c1f5da0dabdee0833e77f7
yiqing <365314342@qq.com>
2024-04-18 20:40:35 +0800

    websocket测试

--- Assets/Deer/Scripts/Hotfix/HotfixBusiness/UI/UIMainMenu/UIMainMenuForm.cs
+++ Assets/Deer/Scripts/Hotfix/HotfixBusiness/UI/UIMainMenu/UIMainMenuForm.cs
@@ -15,6 +15,8 @@ using Main.Runtime;
 using Main.Runtime.Procedure;
 using UnityEngine;
 using UnityGameFramework.Runtime;
+using UnityEngine.Networking;
+using System;
 
 namespace HotfixBusiness.UI
 {
@@ -23,53 +25,108 @@ namespace HotfixBusiness.UI
 	/// </summary>
 	public partial class UIMainMenuForm : UIFixBaseForm
 	{
-		protected override void OnInit(object userData) {
-			 base.OnInit(userData);
-			 GetBindComponents(gameObject);
+		private int connectCount = 0;
+		private readonly int maxReconnectCount = 3;
 
-/*--------------------Auto generate start button listener.Do not modify!--------------------*/
+		protected override void OnInit(object userData)
+		{
+			base.OnInit(userData);
+			GetBindComponents(gameObject);
+
+			/*--------------------Auto generate start button listener.Do not modify!--------------------*/
 			m_Btn_DeerExample.onClick.AddListener(Btn_DeerExampleEvent);
 			m_Btn_DeerGame.onClick.AddListener(Btn_DeerGameEvent);
-/*--------------------Auto generate end button listener.Do not modify!----------------------*/
+			/*--------------------Auto generate end button listener.Do not modify!----------------------*/
 		}
 
 		private void Btn_DeerExampleEvent()
 		{
-			if (!DeerSettingsUtils.DeerGlobalSettings.m_UseDeerExample)
-			{
-				DialogParams dialogParams = new DialogParams();
-				dialogParams.Mode = 1;
-				dialogParams.Title = "提示";
-				dialogParams.Message = "Deer例子已经被移除! [DeerTools/DeerExample/AddExample]可以添加Deer例子。";
-				dialogParams.ConfirmText = "确定";
-				GameEntry.UI.OpenDialog(dialogParams);
-				return;
-			}
-			if (GameEntry.Procedure.CurrentProcedure is ProcedureBase procedureBase)
-			{
-				procedureBase.ProcedureOwner.SetData<VarString>("nextProcedure", Constant.Procedure.ProcedureADeerExample);
-				procedureBase.ChangeStateByType(procedureBase.ProcedureOwner,typeof(ProcedureCheckAssets));
-			}
+			// if (!DeerSettingsUtils.DeerGlobalSettings.m_UseDeerExample)
+			// {
+			// 	DialogParams dialogParams = new DialogParams();
+			// 	dialogParams.Mode = 1;
+			// 	dialogParams.Title = "提示";
+			// 	dialogParams.Message = "Deer例子已经被移除! [DeerTools/DeerExample/AddExample]可以添加Deer例子。";
+			// 	dialogParams.ConfirmText = "确定";
+			// 	GameEntry.UI.OpenDialog(dialogParams);
+			// 	return;
+			// }
+			// if (GameEntry.Procedure.CurrentProcedure is ProcedureBase procedureBase)
+			// {
+			// 	procedureBase.ProcedureOwner.SetData<VarString>("nextProcedure", Constant.Procedure.ProcedureADeerExample);
+			// 	procedureBase.ChangeStateByType(procedureBase.ProcedureOwner, typeof(ProcedureCheckAssets));
+			// }
+			StartCoroutine(IRequestSeverList());
 		}
 
 		private void Btn_DeerGameEvent()
 		{
-			if (!DeerSettingsUtils.DeerGlobalSettings.m_UseDeerExample)
+			// if (!DeerSettingsUtils.DeerGlobalSettings.m_UseDeerExample)
+			// {
+			// 	DialogParams dialogParams = new DialogParams();
+			// 	dialogParams.Mode = 1;
+			// 	dialogParams.Title = "提示";
+			// 	dialogParams.Message = "Deer游戏例子已经被移除! [DeerTools/DeerExample/AddExample]可以添加Deer游戏例子。";
+			// 	dialogParams.ConfirmText = "确定";
+			// 	GameEntry.UI.OpenDialog(dialogParams);
+			// 	return;
+			// }
+			// if (GameEntry.Procedure.CurrentProcedure is ProcedureBase procedureBase)
+			// {
+			// 	procedureBase.ProcedureOwner.SetData<VarString>("nextProcedure", Constant.Procedure.ProcedureAGameExample);
+			// 	procedureBase.ChangeStateByType(procedureBase.ProcedureOwner, typeof(ProcedureCheckAssets));
+			// }
+			StartCoroutine(IRequestSeverList());
+		}
+
+		private IEnumerator IRequestSeverList()
+		{
+			connectCount = 0;
+			bool connectFinish = false;
+
+			while (!connectFinish)
 			{
-				DialogParams dialogParams = new DialogParams();
-				dialogParams.Mode = 1;
-				dialogParams.Title = "提示";
-				dialogParams.Message = "Deer游戏例子已经被移除! [DeerTools/DeerExample/AddExample]可以添加Deer游戏例子。";
-				dialogParams.ConfirmText = "确定";
-				GameEntry.UI.OpenDialog(dialogParams);
-				return;
-			}
-			if (GameEntry.Procedure.CurrentProcedure is ProcedureBase procedureBase)
-			{
-				procedureBase.ProcedureOwner.SetData<VarString>("nextProcedure", Constant.Procedure.ProcedureAGameExample);
-				procedureBase.ChangeStateByType(procedureBase.ProcedureOwner,typeof(ProcedureCheckAssets));
+				using UnityWebRequest webRequest = UnityWebRequest.Get("https://rpg-wx.fjbgwl.com:35002/");
+				yield return webRequest.SendWebRequest();
+				if (!string.IsNullOrEmpty(webRequest.error))
+				{
+					Log.Error("获取服务器列表失败 error = " + webRequest.error);
+					connectCount++;
+					if (connectCount >= maxReconnectCount)
+					{
+						connectFinish = true;
+					}
+					yield return new WaitForSeconds(0.5f);
+				}
+				else
+				{
+					connectFinish = true;
+					Servers servers = JsonUtility.FromJson<Servers>(webRequest.downloadHandler.text);
+					// serverList = servers.gateServers;
+
+					if (servers.gateServers.Count > 0)
+					{
+						GameEntry.WebSocket.ConnectNet(servers.gateServers[0].ip, servers.gateServers[0].port);
+					}
+				}
 			}
 		}
-/*--------------------Auto generate footer.Do not add anything below the footer!------------*/
+		/*--------------------Auto generate footer.Do not add anything below the footer!------------*/
 	}
+}
+
+[Serializable]
+public class ServerInfo
+{
+	public int cellId;
+	public int id;
+	public string ip;
+	public string name;
+	public int port;
+}
+
+[Serializable]
+public class Servers
+{
+	public List<ServerInfo> gateServers;
 }
